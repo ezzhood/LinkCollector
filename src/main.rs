754affcb5e5a1b4b00a04521935e15c5887a216c
eziz-hudayberdiyev <ezizhudayberdiyev123456@gmail.com>
2022-11-25 16:43:28 +0500

    Links are crawled parallelly

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
-use select::{document::Document, predicate::Name};
+use scraper::{Html, Selector};
+use std::cmp::min;
 use std::net::TcpListener;
-use std::sync::mpsc::channel;
+use std::sync::{Arc, Mutex};
 use std::thread::{self, available_parallelism};
 use std::time::Instant;
-
 #[derive(serde::Serialize)]
 struct LinkResponse {
     internal_links: Vec<String>,
@@ -12,97 +12,121 @@ struct LinkResponse {
     time_elapsed: u64,
 }
 
+#[derive(serde::Deserialize)]
+struct LinkQuery {
+    url: String,
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let server =
         HttpServer::new(move || App::new().route("/links", web::get().to(links_get_handler)))
+            // .workers(1)
             .listen(TcpListener::bind("0.0.0.0:4000").unwrap())?
             .run();
 
     server.await
 }
 
-async fn links_get_handler() -> HttpResponse {
-    let host = "https://serpay.penjire.com";
-    let thread_max_number = available_parallelism().unwrap().get();
+async fn links_get_handler(query: web::Query<LinkQuery>) -> HttpResponse {
+    // maximum number of threads to know how many parallel works can be done
+    let max_thread_number = available_parallelism().unwrap().get();
+    // host is prefixed in parallel task so value of it shared among all threads
+    let host = Arc::new(Mutex::new(query.url.to_owned()));
+
+    // internal links are read among all threads
+    let internal_links = Arc::new(Mutex::new(vec![host.lock().unwrap().to_owned()]));
+    // external links are mutated in parallel job
+    let external_links = Arc::new(Mutex::new(vec![]));
 
     let mut has_finished = false;
     let mut pointer = 0;
 
-    let mut internal_links: Vec<String> = vec![host.to_owned()];
-    let mut external_links: Vec<String> = vec![];
-
-    let (tx, rx) = channel();
-
     let start_time = Instant::now();
 
     while !has_finished {
-        let mut recieve_count = 0;
+        let mut thread_handlers = vec![];
+        let internal_links = Arc::clone(&internal_links);
 
-        for i in 0..thread_max_number {
-            let tx = tx.clone();
+        let new_found_internals: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(vec![]));
+        let mut internal_links_vec = internal_links.lock().unwrap();
 
-            if let Some(url) = internal_links.get(pointer + i) {
+        let loop_number = min(max_thread_number, internal_links_vec.len() - pointer);
+
+        for i in 0..loop_number {
+            if let Some(url) = internal_links_vec.get(pointer + i) {
+                let external_links = Arc::clone(&external_links);
+                let new_found_internals = Arc::clone(&new_found_internals);
+                let host = Arc::clone(&host);
                 let url = url.clone();
 
-                thread::spawn(move || {
+                let handle = thread::spawn(move || {
                     let res_text = reqwest::blocking::get(url).unwrap().text().unwrap();
 
-                    let internals_tuple = get_links_from_url(res_text);
-                    tx.send(internals_tuple).unwrap();
+                    let (new_internals, new_externals) =
+                        get_links_from_url(&host.lock().unwrap(), res_text);
+
+                    let mut external_links_vec = external_links.lock().unwrap();
+
+                    for new_external_link in new_externals {
+                        if !external_links_vec.contains(&new_external_link) {
+                            external_links_vec.push(new_external_link);
+                        }
+                    }
+
+                    let mut new_found_internals_vec = new_found_internals.lock().unwrap();
+
+                    for new_internal_link in new_internals {
+                        new_found_internals_vec.push(new_internal_link);
+                    }
                 });
-                recieve_count += 1;
+
+                thread_handlers.push(handle);
             } else {
-                break;
+                continue;
             }
         }
 
-        for _ in 0..recieve_count {
-            if let Ok((new_internals, new_externals)) = rx.recv() {
-                for new_external_link in new_externals {
-                    if !external_links.contains(&new_external_link) {
-                        external_links.push(new_external_link);
-                    }
-                }
+        for handle in thread_handlers {
+            handle.join().unwrap();
+        }
 
-                for new_internal_link in new_internals {
-                    if !internal_links.contains(&new_internal_link) {
-                        internal_links.push(new_internal_link);
-                    }
-                }
+        pointer = internal_links_vec.len();
 
-                if pointer == internal_links.len() - 1 {
-                    has_finished = true;
-                } else {
-                    pointer += 1;
-                }
+        let new_found_internals_vec = new_found_internals.lock().unwrap();
+
+        for new_internal_link in new_found_internals_vec.iter() {
+            if !internal_links_vec.contains(&new_internal_link) {
+                internal_links_vec.push(new_internal_link.to_owned());
             }
+        }
+
+        if pointer == internal_links_vec.len() {
+            has_finished = true;
         }
     }
 
     let result = LinkResponse {
-        internal_links,
-        external_links,
+        internal_links: internal_links.lock().unwrap().to_owned(),
+        external_links: external_links.lock().unwrap().to_owned(),
         time_elapsed: start_time.elapsed().as_secs(),
     };
-
-    // println!("{:#?}", res_text);
 
     HttpResponse::Ok().json(result)
 }
 
-fn get_links_from_url(html_text: String) -> (Vec<String>, Vec<String>) {
+fn get_links_from_url(host: &str, html_text: String) -> (Vec<String>, Vec<String>) {
     let mut external_links: Vec<String> = vec![];
     let mut internal_links: Vec<String> = vec![];
-    Document::from(html_text.as_str())
-        .find(Name("a"))
-        .filter_map(|n| n.attr("href"))
-        .map(|x| x)
+
+    Html::parse_document(&html_text)
+        .select(&Selector::parse("a").unwrap())
+        .filter_map(|n| n.value().attr("href"))
         .for_each(|link| {
             if link.starts_with("http") {
                 external_links.push(String::from(link))
-            } else if link != "/" {
-                internal_links.push("https://serpay.penjire.com".to_owned() + link)
+            } else {
+                internal_links.push(host.to_owned() + link)
             }
         });
 
